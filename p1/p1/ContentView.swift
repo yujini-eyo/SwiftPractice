e4600d5d31ea342146de31f8babae04cd4b32302
yujini-eyo <salt101050@gmail.com>
2026-02-03 11:41:35 +0900

    초기 세팅

---
+++ p1/p1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  p1
+//
+//  Created by yujin on 2/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ p1/p1/p1App.swift
@@ -0,0 +1,17 @@
+//
+//  p1App.swift
+//  p1
+//
+//  Created by yujin on 2/3/26.
+//
+
+import SwiftUI
+
+@main
+struct p1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
