9e44052f543bd255c6d061cd05e6fad3dbac5230
yujini-eyo <salt101050@gmail.com>
2026-02-03 19:12:22 +0900

    5,6강

---
+++ Basics.playground/Contents.swift
@@ -0,0 +1,82 @@
+import UIKit
+
+//문자 자료형
+
+//var variableName = initialValue ->
+/*var name = "Swift" //String Literal ""
+var thisYear = 2026
+var valid = true
+
+//이름 정의 규칙
+//Nameing Convention -> CamelCase
+
+name
+print(name)
+print(thisYear)
+print(valid)
+
+name = "Steve" //-> 변수의 값은 새로운 값으로 재저장
+name = "윤아"
+print(name)
+
+var anotherName = name
+anotherName = "Tim"
+
+print(name, anotherName)
+
+// thisYear = "2034" -> 타입이 다른 변수 저장 안됌
+
+var address = "서울" //let은 값이 다르면 안됌
+address
+
+address = "대구"
+*/
+
+
+//숫자 자료형
+/* 123
+-123
+1.23
+0.23
+1.23e4
+
+0xAp2
+
+100_000_000
+
+10
+0b1010 //->2진수
+
+0o12 //->8진수
+
+0xA //->16진수
+
+//int8 type에 저장될 수 있는 최대최소
+Int8.min
+Int8.max
+*/
+
+
+//불린->Boolean
+/*
+let isHappy = true
+let gloomy: Bool = false
+
+// 형식 추론 -> Type Inference
+let num = 123
+print(type(of: num))
+
+let temp = 123.2
+print(type(of: temp))
+
+let lang = "Swift"
+print(type(of: lang))
+
+
+let a = true
+let b = false
+
+print(type(of: a))
+print(type(of: b))*/
+
+// 자료형 1 3:26:40
---
+++ WarmingUP.playground/Contents.swift
@@ -0,0 +1,50 @@
+import UIKit
+
+//Token
+2+3;
+
+if true {
+    
+}
+
+//Expression -> 표현식
+//하나 이상의 토큰이 모여서 하나의 값을 도출하는 코두
+//evaluate
+
+var x = 7
+x
+
+x + 1
+//Arithmetic Expression
+// 산술 표현식
+
+x < 10
+//Boolean expression
+//논리 표현식
+
+//Statement
+// 하나 이상의 표현식이 모여서 특정 작업을 실행하는 코드
+//if 조건문
+
+//for 문
+
+7
+//리터럴 = Literal
+
+x = 5 + 7
+
+let x2 = x > 7
+
+
+// Identifier -> ID, 식별자
+let number = 1
+let Number = 2
+let _Number = 3
+
+let 😀 = 7
+let 학 = 9
+
+//Keyword -> 식별자 X
+var letnumber = 7
+
+
