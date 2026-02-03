02c1c916f37c17033d022eb3624aae69a58e7031
yujini-eyo <salt101050@gmail.com>
2026-02-03 17:42:55 +0900

    4과

---
+++ HelloXCode/HelloXCode/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  HelloXCode
+//
+//  Created by yujin on 2/3/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        var a = 10
+        a = 20
+        print(a)
+    }
+
+
+}
+
