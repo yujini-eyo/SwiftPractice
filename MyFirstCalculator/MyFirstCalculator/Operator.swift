a943fae2051810607f227a246cb443ce1ba4857a
yujini-eyo <salt101050@gmail.com>
2026-02-05 22:14:15 +0900

    20260205

---
+++ MyFirstCalculator/MyFirstCalculator/Operator.swift
@@ -0,0 +1,8 @@
+//
+//  Operator.swift
+//  MyFirstCalculator
+//
+//  Created by yujin on 2/5/26.
+//
+
+import Foundation
---
+++ SunnyDay/SunnyDay/Model/Weather.swift
@@ -0,0 +1,8 @@
+//
+//  Weather.swift
+//  SunnyDay
+//
+//  Created by yujin on 2/5/26.
+//
+
+import Foundation
